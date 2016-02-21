ba6d70345e8c527d6bc8fa52c4a32c62ebd50f67
Benjamin Downs <benjamin.downs@gmail.com>
2016-02-21 22:27:15 +0100

    version 2.0 finished; added more views and error handling

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ angular.module('wikiApp', ['ngSanitize'])
 
     var wikiService = {
 
-
         get: function(query) {
             // var random = new RegExp('generator=random')
             if (/generator\=random/.test(query)) {
@@ -12,16 +11,26 @@ angular.module('wikiApp', ['ngSanitize'])
             } else {
                 return $http.jsonp('http://en.wikipedia.org/w/api.php?titles=' + query + '&rawcontinue=true&action=query&format=json&prop=extracts&callback=JSON_CALLBACK')
             }
+        },
+
+        getList: function(query) {
+            var url = 'http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + query + '&callback=JSON_CALLBACK'
+            return $http.jsonp(url)
+                
         }
     }
     return wikiService
 }])
+
 
 .controller("wikiCtrl", ["$scope", "wikiService", function($scope, wikiService) {
     $scope.query;
     $scope.wikiResponse;
     $scope.dataReceived = false
     $scope.badQuery = false
+    $scope.content
+    $scope.list
+    $scope.random
 
     $('#queryInput').bind('keypress', function(e) {
         if (e.keyCode == 13) {
@@ -29,12 +38,20 @@ angular.module('wikiApp', ['ngSanitize'])
         }
     });
 
+    $('#contentToggle').change(function() {
+        $scope.content = !$scope.content
+        $scope.$apply()
+    });
+
     $scope.wikiSearch = function() {
 
         if (!$scope.query) {
             $scope.query = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=extracts&callback=JSON_CALLBACK'
+            $scope.list = ""
+            $scope.random = true
         } else {
             $scope.query = $scope.query
+            $scope.random = false
         }
 
         wikiService.get($scope.query)
@@ -45,12 +62,13 @@ angular.module('wikiApp', ['ngSanitize'])
                     $scope.dataReceived = false
                     $scope.badQuery = false
                     $scope.moreReading = ''
-
                     var responseObject = data.data.query.pages
                     $.each(responseObject, function(index, value) {
 
                         if (data.data.query.pages[-1]) {
+                            $scope.list = ''
                             return $scope.badQuery = true;
+
                         }
 
                         $scope.wikiResponse = value.extract
@@ -69,6 +87,13 @@ angular.module('wikiApp', ['ngSanitize'])
                     $scope.dataReceived = false
 
                 })
+
+        wikiService.getList($scope.query)
+            .success(function(data) {
+                    var results = data.query.pages;
+                    $scope.list = results
+                    $scope.badQuery = false;
+                })
     }
 
 
@@ -76,17 +101,23 @@ angular.module('wikiApp', ['ngSanitize'])
 
 
 $('.tooltipped').tooltip({
-     placement: "top",
-     trigger: "hover"
+    placement: "top",
+    trigger: "hover"
 });
+
+
 
 
 function showInput() {
     $('#wikiLogo').fadeOut('slow')
     $('#queryInput').show('slow')
+    $('#toggleDiv').fadeIn().css('display', 'inline-block')
+    $('#contentToggle').bootstrapToggle({
+        on: 'List View',
+        off: 'Content View'
+    })
 }
 
 setTimeout(function() {
     showInput()
 }, 2000)
-
